27a82fd74068df6c9f1ebdc356509ce8109875d3
vincent-guizot <vincentguizot128@gmail.com>
2026-05-31 17:46:33 +0700

    refactoring

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -6,6 +6,10 @@ import "react-loading-skeleton/dist/skeleton.css";
 import "@/styles/global.css";
 
 import App from "./app/App.jsx";
+import { useThemeStore } from "@/features/theme/theme.store";
+
+// bootstrap theme saat app load
+useThemeStore.getState().setTheme(useThemeStore.getState().theme);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
